64f0a40222b1d306995efecf43ad9f2e0120449b
RazGrom <grom02.03.5@mail.ru>
2020-09-17 23:20:09 +0300

    test1

--- discordbot/const.py
+++ discordbot/const.py
@@ -1,3 +1,3 @@
-TOKEN = "NzM0MzU1OTEyNjM0MDA3NTUz.XxQk_A.GBEZV8Zi6rcJINlnD1Gy7CBmCsk"
+TOKEN = "NzM0MzU1OTEyNjM0MDA3NTUz.XxQgPQ.OIHr1NFJaVAXE669XgHl_6jGfGc"
 JOKE_CHANNEL = 'https://www.youtube.com/channel/UCIhJPoYdx9dScgmh56W8L0g'
 YTB_DIR = r'D:\python\MEMbot\audiodata\ytb'
